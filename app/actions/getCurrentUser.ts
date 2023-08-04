575dfe19b068e0dfd95f03d2ff0c2d261202eba7
Miguel Bravo <miguelbravobalestrini@gmail.com>
2023-08-04 16:29:16 -0500

    Social logins setup

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -24,7 +24,14 @@ export default async function getCurrentUser() {
       return null;
     }
 
-    return(currentUser);
+    // return(currentUser);
+
+    return {
+      ...currentUser,
+      createdAt: currentUser.createdAt.toISOString(),
+      updatedAt: currentUser.updatedAt.toISOString(),
+      emailVerified: currentUser.emailVerified?.toISOString() || null,
+    }
 
   } catch (error: any) {
     return null;
