64a64162954805e3b942aa890135e39ef56458a8
Miguel Bravo <miguelbravobalestrini@gmail.com>
2023-08-15 17:13:52 -0500

    Location selection

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,8 @@ import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
 import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../inputs/CountrySelect';
+import dynamic from 'next/dynamic';
+
 
 enum STEPS {
   CATEGORY = 0,
@@ -45,13 +47,18 @@ export default function RentModal() {
   });
 
   const category = watch('category');
+  const location = watch('location');
+
+  const Map = useMemo(() => dynamic(() => import('../Map'), {
+    ssr: false,
+  }), [location]);
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
       shouldDirty: true,
       shouldTouch: true,
       shouldValidate: true,
-    })
+    });
   };
 
   const onBack = () => {
@@ -101,13 +108,20 @@ export default function RentModal() {
 
   if (step === STEPS.LOCATION) {
     bodyContent = (
-      <div className='flex flex-col gap-8'>
-        <Heading title='Where is your place located' subtitle='Help guests find you!' />
-        <CountrySelect 
-          
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Where is your place located"
+          subtitle="Help guests find you!"
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue('location', value)}
+        />
+        <Map 
+          center={location?.latlng}
         />
       </div>
-    )
+    );
   }
 
   return (
