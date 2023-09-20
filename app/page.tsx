736b9037b225154b482ef7c6f310e2ee51dcfea2
Miguel Bravo <miguelbravobalestrini@gmail.com>
2023-09-20 15:04:04 -0500

    Properties screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
+import getCurrentUser from './actions/getCurrentUser';
+import getListings, { IListingsParams } from './actions/getListings';
+import ClientOnly from './components/ClientOnly';
+import Container from './components/Container';
+import EmptyState from './components/EmptyState';
+import ListingCard from './components/listings/ListingCard';
 
-import getCurrentUser from "./actions/getCurrentUser";
-import getListings from "./actions/getListings";
-import ClientOnly from "./components/ClientOnly";
-import Container from "./components/Container";
-import EmptyState from "./components/EmptyState";
-import ListingCard from "./components/listings/ListingCard";
+interface HomeProps {
+  searchParams: IListingsParams;
+}
 
-export default async function Home() {
-  const listings = await getListings();
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
@@ -15,7 +18,7 @@ export default async function Home() {
       <ClientOnly>
         <EmptyState showReset />
       </ClientOnly>
-    )
+    );
   }
 
   return (
@@ -23,14 +26,16 @@ export default async function Home() {
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {listings.map((listing) => (
-            <ListingCard 
+            <ListingCard
               currentUser={currentUser}
-              key={listing.id} 
-              data={listing} 
+              key={listing.id}
+              data={listing}
             />
           ))}
         </div>
       </Container>
     </ClientOnly>
-  )
-}
+  );
+};
+
+export default Home;
